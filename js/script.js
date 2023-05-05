91bca603124fc0dec3cbeafa1ee81bfb39f03086
Bruno <brunosabrito2@gmail.com>
2023-05-05 14:05:30 -0300

    make a form appear as i click in New Book button. Next step is to link formulario input as variables for a object constructor

---
+++ js/script.js
@@ -0,0 +1,64 @@
+console.log("hi");
+
+/*
+2.1.Criar uma relação do botão que ira abrir um formulário com imputs:
+        Name of the book
+        Author
+        Número de páginas
+        Checkbox se leu ou não
+
+
+     let newBook = id(newBook)
+     ao clicar em newBook {
+   criar elemento form
+   e adicionar no body atravez de tudo
+   criar element imput(titulo) 
+   adicionar no form
+   criar imput (autor)
+   adicionar no form
+   criar numero de paginas
+   criar a checklist 
+   */
+
+let newBook = document.querySelector(".newButton");
+console.log(newBook.textContent);
+newBook.addEventListener("click", function () {
+  //criar element form
+  const formulario = document.createElement("FORM");
+  formulario.setAttribute("id", "formulario");
+  document.body.appendChild(formulario);
+  //h4
+  const novoLivro = document.createElement("h4");
+  novoLivro.textContent = "Novo Livro";
+  formulario.appendChild(novoLivro);
+  //autor
+  const autor = document.createElement("input");
+  autor.setAttribute("id", "autor");
+  autor.setAttribute("type", "text");
+  autor.setAttribute("placeholder", "Autor");
+  formulario.appendChild(autor);
+
+  //input titulo
+  const titulo = document.createElement("input");
+  titulo.setAttribute("id", "titulo");
+  titulo.setAttribute("type", "text");
+  titulo.setAttribute("placeholder", "Titulo");
+  formulario.appendChild(titulo);
+
+  //input número de pg
+  const paginas = document.createElement("input");
+  paginas.setAttribute("id", "paginas");
+  paginas.setAttribute("type", "number");
+  paginas.setAttribute("placeholder", "Número de Páginas");
+  formulario.appendChild(paginas);
+
+  //input checkBox leu ou nãoi
+  const label = document.createElement("label");
+  label.setAttribute("for", "checkBox");
+  label.textContent = "Você ja leu o livro?";
+  formulario.appendChild(label);
+  const checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.setAttribute("id", "checkBox");
+  formulario.appendChild(checkBox);
+});
